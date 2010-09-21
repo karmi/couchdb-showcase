80936fc812ff3eb8eba38fe4629cbde530c0ed97
Karel Minarik <karmi@karmi.cz>
2010-09-21 18:39:53 +0200

    Added example URLS into by_name view

--- couchdb/_design/person/views/by_name/map.js
+++ couchdb/_design/person/views/by_name/map.js
@@ -1,4 +1,11 @@
-// http://localhost:5984/addressbook/_design/person/_view/by_name?key=%22Waters%20Jaquelin%22&reduce=false&include_docs=true
+// ORDERED BY LAST NAME
+// -> http://localhost:5984/addressbook/_design/person/_view/by_name?reduce=false
+
+// REVERSE ORDERED BY LAST NAME
+// -> http://localhost:5984/addressbook/_design/person/_view/by_name?reduce=false&descending=true
+
+// EXACT MATCH
+// -> http://localhost:5984/addressbook/_design/person/_view/by_name?key=%22Waters%20Jaquelin%22&reduce=false&include_docs=true
 
 function(doc) {
   if (doc.last_name && doc.first_name) {
