c04dd416dd151cc2247a83f8dfab8003e7ee2726
Karel Minarik <karmi@karmi.cz>
2010-09-21 19:14:43 +0200

    Added simple HTML _show definition for displaying contact detail
    See:
    * http://guide.couchdb.org/draft/show.html
    * https://developer.mozilla.org/en/Core_JavaScript_1.5_Guide/Processing_XML_with_E4X

---
+++ couchdb/_design/person/shows/detail.js
@@ -0,0 +1,28 @@
+// See:
+// * http://guide.couchdb.org/draft/show.html
+// * https://developer.mozilla.org/en/Core_JavaScript_1.5_Guide/Processing_XML_with_E4X
+
+function(doc, req) {
+  
+  var full_name = doc.first_name + doc.last_name
+
+  doc.phones.toString = function() {
+    var phones = []; for (prop in this) { if ( prop != 'toString' ) phones.push( this[prop] + ' (' + prop + ')' ) }
+    return phones.join(', ')
+  }
+
+    return (<html>
+      <head>
+        <title>{full_name} (Address Book)</title>
+      </head>
+      <body>
+        <h1>{full_name}</h1>
+        <p>Occupation:      {doc.occupation}</p>
+        <p>Birthday:        {doc.birthday}</p>
+        <p>Groups:          {doc.groups.join(', ')}</p>
+        <p>Phone numbers:   {doc.phones}</p>
+      </body>
+    </html>).
+      toXMLString()
+
+}
